7a76e2a622709925298be147136564f633aecf1b
k17h02 <k17h02@gmail.com>
2021-09-30 15:03:34 +0600

    nav change

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container >
-                <Navbar.Brand href="/home">মজার খাবার</Navbar.Brand>
+                <Navbar.Brand >মজার খাবার</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
